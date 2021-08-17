382aec66dd64be324f553b038b6ec3f0e76f5348
bluu0010 <bluu0010@student.monash.edu>
2021-08-17 11:14:12 +1000

    add favicon

--- venv/app.py
+++ venv/app.py
@@ -1,11 +1,18 @@
+import os
+
 from flask import Flask
 from flask import render_template
+from flask import send_from_directory
 # app = Flask(__name__)
 app = Flask(__name__, static_folder="templates/static")
 
 @app.route("/")
 def home():
     return render_template('views/Home.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, "templates"), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- venv/templates/static/js/Header.js
+++ venv/templates/static/js/Header.js
@@ -1,8 +1,13 @@
 function importHeader() {
-    document.write(`
-    <h1 id="header">Header</h1>
+  document.write(`
+    <div id="header">
+    <nav class="d-flex justify-content-evenly">
+        <h1>Feature 1</h1>
+        <h1>Feature 2</h1>
+        <h1>Feature 3</h1>
+    </nav>
+    </div>
     `);
-
 }
 
 importHeader();
