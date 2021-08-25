601cd6e506beeff1ba1bb3a0bdf97dcb516eca6a
Boxuan <bluu0010@student.monash.edu>
2021-08-25 18:59:42 +1000

    modify resources page, add resources.css

--- venv/templates/static/js/Head.js
+++ venv/templates/static/js/Head.js
@@ -1,6 +1,5 @@
 function importHead() {
   document.write(`
-    <head>
     <meta charset="UTF-8" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <script src="https://kit.fontawesome.com/da2ea2dbe9.js" crossorigin="anonymous"></script>
@@ -9,7 +8,6 @@ function importHead() {
     <link rel="stylesheet" href="/static/css/main-content.css" />
     <link rel="stylesheet" href="/static/css/footer.css" />
     <title>5 Nuts - BE SAFE, BE HEALTHY</title>
-    </head>
     `);
 }
 
