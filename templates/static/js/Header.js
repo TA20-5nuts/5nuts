d51efce605abbbead273f879f6da38912b0cf13f
Boxuan <bluu0010@student.monash.edu>
2021-08-27 16:25:05 +1000

    redesign home page

--- templates/static/js/Header.js
+++ templates/static/js/Header.js
@@ -3,39 +3,36 @@ function importHeader() {
   <div id="header">
       <div class="header-item-left">
         <div id="logo">
-          <img src="../static/team-logo.png" alt="logo" class="logo" />
+          <img src="../static/team-logo-transparent.png" alt="logo" class="logo" />
         </div>
         <div>
-          <a href="/home" class="header-link"><h5>5 Nuts</h5></a>
+          <a href="/home" class="header-link"><h5>Nuts&Dairy</h5></a>
         </div>
       </div>
       <div class="header-item-center"></div>
       <div class="header-item-right">
         <div class="header-item">
           <h5>
-            <a class="header-link" href="/introduction"
-              ><i class="fas fa-info"></i> Allergy</a
-            >
+            <a class="header-link" href="/introduction">
+<!--            <i class="fas fa-info"></i>-->
+             Allergy</a>
           </h5>
         </div>
         <div class="header-item">
           <h5>
             <a class="header-link" href="/resources"
-              ><i class="fas fa-book-open"></i> Resources</a
+              >
+<!--              <i class="fas fa-book-open"></i> -->
+              Resources</a
             >
           </h5>
         </div>
         <div class="header-item">
           <h5>
             <a class="header-link" href="/quiz"
-              ><i class="fas fa-diagnoses"></i> Quiz</a
-            >
-          </h5>
-        </div>
-        <div class="header-item">
-          <h5>
-            <a class="header-link" href="/game"
-              ><i class="fas fa-puzzle-piece"></i> Game</a
+              >
+<!--              <i class="fas fa-diagnoses"></i> -->
+              Quiz</a
             >
           </h5>
         </div>
