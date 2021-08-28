740faf48fe4ef9bb0e7c9b2fa42f92040508b125
Boxuan <bluu0010@student.monash.edu>
2021-08-28 15:34:43 +1000

    remove quiz page

--- templates/static/js/HomePage/quiz.js
+++ templates/static/js/HomePage/quiz.js
@@ -1,4 +1,5 @@
 function openQuiz() {
+  clearAns();
   let quiz = document.getElementById("modalQuiz");
   quiz.style.display = "block";
 }
@@ -18,4 +19,18 @@ function checkResult() {
   }
   let quiz = document.getElementById("modalQuiz");
   quiz.style.display = "none";
+}
+
+function clearAns() {
+  let q1Ans = document.getElementsByName("q1");
+  let q2Ans = document.getElementsByName("q2");
+  let q3Ans = document.getElementsByName("q3");
+  let q4Ans = document.getElementsByName("q4");
+
+  let list = [q1Ans, q2Ans, q3Ans, q4Ans];
+  for (let li of list) {
+    for (let l of li) {
+      l.checked = false;
+    }
+  }
 }
