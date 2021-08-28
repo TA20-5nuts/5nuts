14b497813ca5e253ebf6d7b7e0e96684810a6b40
Boxuan <bluu0010@student.monash.edu>
2021-08-28 22:14:54 +1000

    modify css

--- templates/static/js/ResourcePage/charts.js
+++ templates/static/js/ResourcePage/charts.js
@@ -59,7 +59,7 @@ function drawBarChart() {
 
   option = {
     title: {
-      text: "Type of food avoided for cultural, religious or ethical reasons",
+      text: "Type of food avoided for cultural, religious or ethical reasons (age groups)",
       left: "center"
     },
     legend: {
@@ -68,17 +68,16 @@ function drawBarChart() {
     tooltip: {},
     dataset: {
       source: [
-        ['product', '2-8', '19-30', '31-50', '51-70', '71 and over', '19 and over'],
-        ['Meat', 0.9, 2.8, 1.3, 1.5, 0, 1.6],
-        ['Fish', 0.4, 2.3, 0.6, 0.6, 0, 0.9],
-        ['Poultry', 0.6, 2.5, 0.8, 0.9, 0, 1.2],
-        ['Pork', 4.4, 5.1, 4.6, 2.0, 1.2, 3.6],
-        ['Dairy', 0, 0, 0.1, 0.4, 0, 0.2],
-        ['Haram', 0.6, 0.7, 0.9, 0.3, 0.7, 0.7],
-        ['Beef', 0.8, 3.5, 1.7, 0.7, 0.3, 1.7],
-        ['Eggs', 0.3, 1.2, 0.3, 0.6, 0, 0.6],
-        ['All animation products', 0.1, 0.5, 0.7, 0.3, 0, 0.5],
-        ['Other', 0.4, 1.2, 0.9, 1.0, 0.1, 0.9]
+        ['product', '2-18', '19-30'],
+        ['Meat', 0.9, 2.8],
+        ['Fish', 0.4, 2.3],
+        ['Poultry', 0.6, 2.5],
+        ['Pork', 4.4, 5.1],
+        ['Haram', 0.6, 0.7],
+        ['Beef', 0.8, 3.5],
+        ['Eggs', 0.3, 1.2],
+        ['All animation products', 0.1, 0.5],
+        ['Other', 0.4, 1.2]
       ]
     },
     xAxis: {
@@ -94,46 +93,10 @@ function drawBarChart() {
     // to a column of dataset.source by default.
     series: [
       {
-        type: 'bar',
-        itemStyle: {
-          borderRadius: 10,
-          borderColor: '#fff'
-        }
+        type: 'bar'
       },
       {
-        type: 'bar',
-        itemStyle: {
-          borderRadius: 10,
-          borderColor: '#fff'
-        }
-      },
-      {
-        type: 'bar',
-        itemStyle: {
-          borderRadius: 10,
-          borderColor: '#fff'
-        }
-      },
-      {
-        type: 'bar',
-        itemStyle: {
-          borderRadius: 10,
-          borderColor: '#fff'
-        }
-      },
-      {
-        type: 'bar',
-        itemStyle: {
-          borderRadius: 10,
-          borderColor: '#fff'
-        }
-      },
-      {
-        type: 'bar',
-        itemStyle: {
-          borderRadius: 10,
-          borderColor: '#fff'
-        }
+        type: 'bar'
       }
     ]
   };
