aeb68378a8b82b93999c8a001d2856144c7f47dd
Boxuan <bluu0010@student.monash.edu>
2021-08-30 14:27:32 +1000

    upgrade insecure request

--- templates/static/js/Head.js
+++ templates/static/js/Head.js
@@ -1,6 +1,7 @@
 function importHead() {
   document.write(`
     <meta charset="UTF-8" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <script src="https://cdn.jsdelivr.net/npm/echarts@5.1.2/dist/echarts.min.js"></script>
     <script src="https://kit.fontawesome.com/da2ea2dbe9.js" crossorigin="anonymous"></script>
