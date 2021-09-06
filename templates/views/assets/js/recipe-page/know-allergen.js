6442ce1285d46faf69d6ec0771e8ad9725c2fd5f
Boxuan <bluu0010@student.monash.edu>
2021-09-06 21:22:57 +1000

    toggle known allergen button

--- templates/views/assets/js/recipe-page/know-allergen.js
+++ templates/views/assets/js/recipe-page/know-allergen.js
@@ -1,3 +1,19 @@
-function toggleKnownAllergen() {
-  alert("Toggle");
+/**
+ * toggle button from in-active to active or the other way around
+ * @param id unique identifier
+ * @param active current active status
+ */
+function toggleKnownAllergen(id, active) {
+  let selectedBtn = document.getElementById(id);
+
+  let isActive = false;
+  if (selectedBtn.className == "icon-box-active") {
+    isActive = true;
+  }
+
+  if (isActive) {
+    selectedBtn.setAttribute("class", "icon-box");
+  } else {
+    selectedBtn.setAttribute("class", "icon-box-active");
+  }
 }
