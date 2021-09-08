27c5ddea49526b06e699c27f25f9f8e4e09a7aab
Boxuan <bluu0010@student.monash.edu>
2021-09-08 15:45:06 +1000

    lunchbox design

--- templates/views/assets/js/recipe-page/know-allergen.js
+++ templates/views/assets/js/recipe-page/know-allergen.js
@@ -1,3 +1,5 @@
+let knownAllergens = []; // a list to store all selected known allergens
+
 /**
  * toggle button from in-active to active or the other way around
  * @param id unique identifier
@@ -5,6 +7,8 @@
  */
 function toggleKnownAllergen(id, active) {
   let selectedBtn = document.getElementById(id);
+  let allergen = selectedBtn.innerText;
+  console.log(allergen);
 
   let isActive = false;
   if (selectedBtn.className == "icon-box-active") {
@@ -13,7 +17,19 @@ function toggleKnownAllergen(id, active) {
 
   if (isActive) {
     selectedBtn.setAttribute("class", "icon-box");
+    const index = knownAllergens.indexOf(allergen);
+    knownAllergens.splice(index, 1); // remove for array
   } else {
     selectedBtn.setAttribute("class", "icon-box-active");
+    knownAllergens.push(allergen); // add to array
   }
+}
+
+/**
+ * select different type of foods for lunchbox
+ * @param id lunchbox item
+ * @param selected if the item has been selected
+ */
+function selectFood(id, selected) {
+  console.log(id);
 }
